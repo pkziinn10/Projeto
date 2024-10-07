ae741cebaa69a006037b32b57b400af38127bd67
PK <137650298+pkziinn10@users.noreply.github.com>
2024-10-07 11:15:58 -0300

    Update exercicio1.cs

--- Projeto 1/exercicio1.cs
+++ Projeto 1/exercicio1.cs
@@ -1,7 +1,3 @@
-    Questão 1
-    Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
-    mensagem explicativa, conforme exemplos.
-
 ﻿using System;
 using System.Globalization;
 namespace program {
