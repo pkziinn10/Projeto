d970c4f2bde0bd3a20552d76a8d9aa0faf56bf4e
PK <137650298+pkziinn10@users.noreply.github.com>
2024-10-07 11:26:35 -0300

    Update exercicio6.cs

--- Projeto 1/exercicio6.cs
+++ Projeto 1/exercicio6.cs
@@ -3,31 +3,27 @@ using System.Globalization;
 namespace program {
     class Program {
         static void Main(String[] args) {
-             int codigo, qnt,codigo2 , qnt2;
-            decimal preco, preco2, valor1, valor2, soma;
+            float A, B, C;
+            double triangulo, circulo,trapezio,quadrado,retangulo;
 
-            Console.WriteLine("Informe o código do produto:");
-            codigo = int.Parse(Console.ReadLine());
-            Console.WriteLine("Informe a quantidade do produto:");
-            qnt = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o preço unitário");
-            preco = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor de A:");
+            A = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor de B:");
+            B = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor de C:");
+            C = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Informe o código do produto 2:");
-            codigo2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Informe a quantidade do produto 2:");
-            qnt2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o preço unitário do produto 2:");
-            preco2 = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            triangulo = (A * C) / 2;
+            circulo = 3.14159 * (C * C);
+            trapezio =  (A + B) * C / 2;
+            quadrado = B * B;
+            retangulo = A * B;
 
-            valor1 = qnt * preco;
-            valor2 = qnt2 * preco2;
-            
-            Console.WriteLine("Valor peça 1: " + valor1.ToString("F2",CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor peça 2: " + valor2.ToString("F2", CultureInfo.InvariantCulture));
-
-            soma = valor1 + valor2;
-            Console.WriteLine("Valor da soma dos 2 produtos: " + "R$" +soma.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(("Triângulo: ")+ (triangulo.ToString("F3",CultureInfo.InvariantCulture)));
+            Console.WriteLine(("Circulo: ") + (circulo.ToString("F3", CultureInfo.InvariantCulture)));
+            Console.WriteLine(("Trapézio: ") + (trapezio.ToString("F3", CultureInfo.InvariantCulture)));
+            Console.WriteLine(("Quadrado: ") + (quadrado.ToString("F3", CultureInfo.InvariantCulture)));
+            Console.WriteLine(("Retângulo: ") + (retangulo.ToString("F3", CultureInfo.InvariantCulture)));
         }
     }
 }
