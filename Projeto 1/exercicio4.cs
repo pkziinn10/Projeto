864c686fb838dca584b1eb90d6fd45e9ef6aa8e2
PK <137650298+pkziinn10@users.noreply.github.com>
2024-10-07 11:22:19 -0300

    Update exercicio4.cs

--- Projeto 1/exercicio4.cs
+++ Projeto 1/exercicio4.cs
@@ -1,1 +1,15 @@
-
+﻿using System;
+using System.Globalization;
+namespace program {
+    class Program {
+        static void Main(String[] args) {
+            int n1, n2, x;
+            Console.WriteLine("Digite um numero:");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o numero por quanto quer somar:");
+            n2 = int.Parse(Console.ReadLine()); 
+            x = n1 + n2;
+            Console.WriteLine("Valor da soma:"+x);
+        }
+    }
+}
