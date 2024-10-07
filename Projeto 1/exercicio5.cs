22da807f73275d460844f0b6b005f7d850d6cff8
PK <137650298+pkziinn10@users.noreply.github.com>
2024-10-07 11:25:22 -0300

    Update exercicio5.cs

--- Projeto 1/exercicio5.cs
+++ Projeto 1/exercicio5.cs
@@ -3,19 +3,31 @@ using System.Globalization;
 namespace program {
     class Program {
         static void Main(String[] args) {
-            int numFuncionario;
-            double salario, hrsTrabalhadas, quantiaHR;
+             int codigo, qnt,codigo2 , qnt2;
+            decimal preco, preco2, valor1, valor2, soma;
 
-            Console.WriteLine("Digite o número do funcionário:");
-            numFuncionario = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite suas horas trabalhadas:");
-            hrsTrabalhadas = double.Parse(Console.ReadLine());
-            Console.WriteLine("Digite quanto que você:");
-            quantiaHR = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Informe o código do produto:");
+            codigo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade do produto:");
+            qnt = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o preço unitário");
+            preco = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            salario = quantiaHR * hrsTrabalhadas;
-            Console.WriteLine("Numero do funcionário: " + numFuncionario);
-            Console.WriteLine(("O sálario do funcionário e: ")+"U$"+salario.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Informe o código do produto 2:");
+            codigo2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade do produto 2:");
+            qnt2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o preço unitário do produto 2:");
+            preco2 = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            valor1 = qnt * preco;
+            valor2 = qnt2 * preco2;
+            
+            Console.WriteLine("Valor peça 1: " + valor1.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor peça 2: " + valor2.ToString("F2", CultureInfo.InvariantCulture));
+
+            soma = valor1 + valor2;
+            Console.WriteLine("Valor da soma dos 2 produtos: " + "R$" +soma.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
