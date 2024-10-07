260835e0d3b88128bbcd67c3db679632a96668a5
PK <137650298+pkziinn10@users.noreply.github.com>
2024-10-07 11:18:49 -0300

    Update exercicio2.cs

--- Projeto 1/exercicio2.cs
+++ Projeto 1/exercicio2.cs
@@ -1,9 +1,3 @@
-Questão 2
-    Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
-    casas decimais conforme exemplos.
-    Fórmula da área: area = π . raio2
-    Considere o valor de π = 3.14159
-        
 ﻿using System;
 using System.Globalization;
 namespace program {
