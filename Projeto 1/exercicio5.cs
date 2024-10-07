67ace520733ed8ddeef0da678bfda0879621a19e
PK <137650298+pkziinn10@users.noreply.github.com>
2024-10-07 11:24:40 -0300

    Update exercicio5.cs

--- Projeto 1/exercicio5.cs
+++ Projeto 1/exercicio5.cs
@@ -1,1 +1,21 @@
+﻿using System;
+using System.Globalization;
+namespace program {
+    class Program {
+        static void Main(String[] args) {
+            int numFuncionario;
+            double salario, hrsTrabalhadas, quantiaHR;
 
+            Console.WriteLine("Digite o número do funcionário:");
+            numFuncionario = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite suas horas trabalhadas:");
+            hrsTrabalhadas = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite quanto que você:");
+            quantiaHR = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = quantiaHR * hrsTrabalhadas;
+            Console.WriteLine("Numero do funcionário: " + numFuncionario);
+            Console.WriteLine(("O sálario do funcionário e: ")+"U$"+salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
